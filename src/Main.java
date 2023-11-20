fae58f899f21891d385fc5d55044fbbaf860eb32
Samboduy <samdanielsson000@gmail.com>
2023-11-20 14:46:44 +0100

    HEJDÅ

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hej sambodaj och viktor");
+
+
+        System.out.println("Hej");
+        System.out.println("då");
+
     }
 }
