41d183c61e2737a0229f08d9c3c3bc0d51ae3c11
OliverkNilsson <oliverknilsson@gmail.com>
2023-11-21 12:17:28 +0100

    La till title och "status"

--- src/Window.java
+++ src/Window.java
@@ -10,6 +10,22 @@ Window () {
     setLayout(new BorderLayout());
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setSize(1000, 750);
+
+    JPanel topPanel = new JPanel();
+    add(topPanel, BorderLayout.NORTH);
+
+    JPanel topPanelTitle = new JPanel();
+    topPanel.add(topPanelTitle, BorderLayout.NORTH);
+
+    JLabel title = new JLabel("Tic Tac Toe!");
+    topPanelTitle.add(title);
+
+    JPanel topPanelStatus = new JPanel();
+    add(topPanelStatus, BorderLayout.SOUTH);
+
+    JLabel status = new JLabel("status");
+    topPanelStatus.add(status);
+
     setVisible(true);
 }
 
