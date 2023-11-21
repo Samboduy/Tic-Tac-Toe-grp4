c8141261db670b0067f5a04f48d813acd8355181
AleksanderBjelk <bjelkaleksander@gmail.com>
2023-11-21 11:47:03 +0100

    Skapar ett fönster/JFrame

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        Window window = new Window();
+
 //Controll T är pull kortkommando
 
 
--- src/Window.java
+++ src/Window.java
@@ -1,5 +1,13 @@
-public class Window {
+import javax.swing.*;
+
+public class Window extends JFrame {
     //BorderLayout
     //En JPanel
     //Sätta text i NORTH med JLabel
+
+Window () {
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(1000, 750);
+    setVisible(true);
+}
 }
