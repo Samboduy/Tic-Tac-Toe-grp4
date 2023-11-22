903b90ac0520c92a8cb68c84067388e5664852a4
olesien <olesien2@gmail.com>
2023-11-22 11:04:54 +0100

    Started adding hover on squares

--- src/Square.java
+++ src/Square.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 
 public class Square extends JButton {
     private char marker;
@@ -10,17 +10,40 @@ public class Square extends JButton {
     Square(PlayingField playingField) {
         this.playingField = playingField;
         upptagen = false;
-        marker = ' ';
+        marker = playingField.getNuvarandeSpelare();
         setFont(new Font("Arial", Font.PLAIN, 40));
         addActionListener(e ->
         {
             if (!upptagen){
-                marker = playingField.getNuvarandeSpelare();
                 setText(Character.toString(marker));
                 upptagen = true;
                 playingField.bytaSpelare();
+
+                marker = playingField.getNuvarandeSpelare();
+                setForeground(new Color(0,0,0,100)); //Override hover
             }
-        });}
+        });
+        addMouseListener(new MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                //setBackground(Color.GREEN);
+
+                if (!upptagen) {
+                    setText(Character.toString(marker));
+                    setForeground(new Color(0,0,0,20));
+                }
+
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                //setBackground(UIManager.getColor("control"));
+                if (!upptagen) {
+                    setText("");
+                    setForeground(new Color(0,0,0,100));
+                }
+            }
+        });
+    }
+
 
     //Anropas från PlayingField. Rensar variabler och texten i square.
     public void rensa () {
